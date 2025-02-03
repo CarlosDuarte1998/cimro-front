af7696a762de72653ee3edc5295253905d347393
CarlosDuarte1998 <hdzalex1998@gmail.com>
2025-02-02 21:58:14 -0600

    Commit initial

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+    import { ref } from 'vue';
+
+
+
+const activeSection = ref(false);
+</script>
+
+<template>
+    <div>
+        <button @click="activeSection = !activeSection">
+            Click me
+        </button>
+
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
