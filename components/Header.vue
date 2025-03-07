17b0728a8c6b57b375084e4e802aa6906825b112
Carlos Duarte <hdzalex1998@gmail.com>
2025-03-07 13:22:59 -0600

    Add font Monserrat

--- components/Header.vue
+++ components/Header.vue
@@ -3,6 +3,13 @@
         <div class="flex justify-between text-white  bg-blue-950 sm:px-20 py-2">
             <div>Jl. Gatot Subroto Barat, Denpasar Utara</div>
             <div>
+                <ul>
+                    <li>
+                        <a href="#" class="hover:text-yellow-500">
+                            <font-awesome icon="user" style="font-size: 30px" />
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -12,24 +19,24 @@
                 <img src="~/assets/images/logo-cimro.png" alt="logo" class="w-38" />
             </div>
             <div class="flex justify-end ">
-                <ul class="flex gap-3">
+                <ul class="flex gap-4 font-medium">
                     <li>
-                        <nuxt-link to="/" class="text-blue-950">Home</nuxt-link>
+                        <nuxt-link to="/" class="">Home</nuxt-link>
                     </li>
                     <li>
-                        <nuxt-link to="/about" class="text-blue-950">Quienes somos</nuxt-link>
+                        <nuxt-link to="/about" class="">Quienes somos</nuxt-link>
                     </li>
                     <li>
-                        <nuxt-link to="/services" class="text-blue-950">Servicios</nuxt-link>
+                        <nuxt-link to="/services" class="">Servicios</nuxt-link>
                     </li>
                     <li>
-                        <nuxt-link to="/contact" class="text-blue-950">Noticias</nuxt-link>
+                        <nuxt-link to="/contact" class="">Noticias</nuxt-link>
                     </li>
                     <li>
-                        <nuxt-link to="/contact" class="text-blue-950">Videos</nuxt-link>
+                        <nuxt-link to="/contact" class="">Videos</nuxt-link>
                     </li>
                     <li>
-                        <nuxt-link to="/contact" class="text-blue-950">Galeria</nuxt-link>
+                        <nuxt-link to="/contact" class="">Galeria</nuxt-link>
                     </li>
                     <li>
                         <nuxt-link to="/contact" class="py-1 px-2  bg-black text-white rounded-sm">Contacto</nuxt-link>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr: false,
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css', '~/assets/css/styles.css'],
   vite: {
     plugins: [
       tailwindcss(),
@@ -16,5 +16,12 @@ export default defineNuxtConfig({
     'v-gsap-nuxt',
     '@vee-validate/nuxt',
     '@nuxtjs/sitemap'
-  ]
+  ],
+  fontawesome: {
+    icons: {
+      solid: ['dollar-sign', 'cog', 'circle', 'check', 'calendar'],
+      brands: ['twitter', 'facebook', 'linkedin', 'github'],
+      regular: ['user'],
+    },
+  },
 })
