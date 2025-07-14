a8d3ba10e458416a138403d48b6ca56dc53b1081
Carlos Alexander Duarte Hernadez <64160994+CarlosDuarte1998@users.noreply.github.com>
2025-07-14 13:42:01 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
-  // SSR habilitado para mejor SEO
+  // SSR habilitado para mejor SEO 
   ssr: true,
 
   ui: {
